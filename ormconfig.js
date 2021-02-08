b30b36ea73e71e9b006e50f5521eeb78cf41b8d6
Paulo Vaz <psgvaz@gmail.com>
2021-02-07 21:19:35 -0300

    first commit

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,23 @@
-console.log(process.env.NODE_ENV ? process.env.TYPEORM_ENTITIES: "dist/models/**/*.js");
-console.log(process.env.NODE_ENV ? process.env.TYPEORM_MIGRATIONS: "dist/migrations/**/*.js");
-console.log(process.env.NODE_ENV ? process.env.TYPEORM_MIGRATIONS_DIR: "dist/migrations/");
+console.log(process.env.NODE_ENV === 'local' ? process.env.TYPEORM_ENTITIES: "dist/models/**/*.js");
+console.log(process.env.NODE_ENV === 'local' ? process.env.TYPEORM_MIGRATIONS: "dist/migrations/**/*.js");
+console.log(process.env.NODE_ENV === 'local'? process.env.TYPEORM_MIGRATIONS_DIR: "dist/migrations/");
+console.log(process.env.NODE_ENV );
+
 
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "synchronize": false,
   "entities": [
-    process.env.NODE_ENV ? process.env.TYPEORM_ENTITIES: "dist/models/**/*.js"
+    process.env.NODE_ENV === 'local' ? process.env.TYPEORM_ENTITIES: "dist/models/**/*.js"
  ],
  "migrations": [
-    process.env.NODE_ENV ? process.env.TYPEORM_MIGRATIONS: "dist/migrations/**/*.js"
+    process.env.NODE_ENV === 'local' ? process.env.TYPEORM_MIGRATIONS: "dist/migrations/**/*.js"
 ],
  "cli":{
   "migrationsDir": [
-    process.env.NODE_ENV ? process.env.TYPEORM_MIGRATIONS_DIR: "dist/migrations/"
+    process.env.NODE_ENV === 'local' ? process.env.TYPEORM_MIGRATIONS_DIR: "dist/migrations/"
   ],
-  "entitiesDir": process.env.NODE_ENV ? process.env.TYPEORM_ENTITIES_DIR: "dist/models"
+  "entitiesDir": process.env.NODE_ENV === 'local' ? process.env.TYPEORM_ENTITIES_DIR: "dist/models"
   }
 }
